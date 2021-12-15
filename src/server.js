8813fa4b60ad1209ef092b69b655a061e420e817
Hasnaa Al-Habahbeh <hasnaahab98@gmail.com>
2021-12-15 17:43:36 +0200

    config

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+const cors = require("cors");
+app.use(cors({ origin: '*' }));
 // Routes:
 const itemRoutes = require('./routes/item.routes');
 app.use(itemRoutes);
